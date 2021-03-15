e27d6a58078acdcdb660224ab49a0ff508d651a1
mhuzaifakhan97 <huzaifanadir1997@gmail.com>
2021-03-15 19:43:47 +0500

    Getx Snackbar.

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_complete/snackBar/snackBar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LearningGetX(),
+    );
+  }
+}
+
+class LearningGetX extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Learning Getx'),
+      ),
+      body: SnackBarExample(),
+    );
+  }
+}
---
+++ lib/snackBar/snackBar.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SnackBarExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Learning Snack Bar using GetX',
+              style: TextStyle(fontSize: 22),
+            ),
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            onPressed: () {
+              Get.snackbar('GetX Snackbar', 'Demo Message from getX Snackbar',
+                  snackPosition: SnackPosition.BOTTOM,
+                  // titleText: Text(
+                  //   'Another Title',
+                  //   style: TextStyle(color: Colors.red, fontSize: 22),
+                  // ),
+                  // messageText: Text(
+                  //   'Another Text from getX Snackbar',
+                  //   style: TextStyle(color: Colors.yellow, fontSize: 22),
+                  // ),
+                  colorText: Colors.yellow,
+                  backgroundColor: Colors.orange,
+                  borderRadius: 100,
+                  maxWidth: 240,
+                  // animationDuration: Duration(milliseconds: 3000),
+                  backgroundGradient:
+                      LinearGradient(colors: [Colors.purple, Colors.orange]),
+                  borderColor: Colors.pink,
+                  borderWidth: 10,
+                  boxShadows: [
+                    BoxShadow(
+                      color: Colors.green,
+                      offset: Offset(1, 2),
+                      spreadRadius: 2,
+                      blurRadius: 3,
+                    )
+                  ],
+                  // isDismissible: true,
+                  // dismissDirection: SnackDismissDirection.HORIZONTAL,
+                  // forwardAnimationCurve: Curves.bounceIn,
+                  // duration: Duration(microseconds: 5000).
+                  icon: Icon(Icons.send),
+                  leftBarIndicatorColor: Colors.blue,
+                  mainButton: TextButton(
+                    onPressed: () {},
+                    child: Text('Retry', style: TextStyle(color: Colors.white)),
+                  ),
+                  onTap: (val) {
+                    print('Snackbar Clicked');
+                    print(val);
+                  },
+                  overlayBlur: 5,
+                  overlayColor: Colors.grey.withOpacity(.3),
+                  padding: EdgeInsets.all(30),
+                  showProgressIndicator: true,
+                  progressIndicatorBackgroundColor: Colors.orange,
+                  progressIndicatorValueColor:
+                      AlwaysStoppedAnimation<Color>(Colors.white),
+                  reverseAnimationCurve: Curves.easeInBack,
+                  snackbarStatus: (val) {
+                    print('Status');
+                    print(val);
+                  },
+                  userInputForm: Form(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(),
+                        )
+                      ],
+                    ),
+                  ));
+            },
+            child: Text(
+              'Show SnackBar',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
